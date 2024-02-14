e29b7e0d110c7355eb9720df389e676a85c0735e
Jefferson <jefferson.brandao@g.globo>
2024-02-14 18:58:11 -0300

    configurando variavel de ambiente

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit'
+import { env } from './src/env'
 
 export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: 'postgres://docker:docker@127.0.0.1:5432/pizza_shop'
+    connectionString: env.DATABASE_URL,
   }
 } satisfies Config
--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,11 +2,15 @@ import postgres from "postgres";
 
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { env } from "../env";
+import chalk from "chalk";
 
-const connection = postgres('postgres://docker:docker@127.0.0.1:5432/pizza_shop', { max: 1 })
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: 'drizzle' })
+
+console.log(chalk.greenBright('Migrations ran successfully'))
 
 await connection.end()
 
